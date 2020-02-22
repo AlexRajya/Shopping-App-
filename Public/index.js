cfaad19cbf8e8c4a1d0b179b89f3f92bef0b6438
AlexRajya <55755818+AlexRajya@users.noreply.github.com>
2020-02-22 11:13:18 +0000

    Login added

--- Public/index.js
+++ Public/index.js
@@ -4,4 +4,16 @@ window.onload = () => {
     let results = document.getElementById('results');
     results.textContent = document.getElementById('searchField').value;//Replace with api result when given API keys
   });
+
+  document.getElementById('login').addEventListener('click', () => {
+    document.getElementById('Modal').style.display = 'block';
+  });
+  document.getElementById('close').addEventListener('click', () => {
+    document.getElementById('Modal').style.display = 'none';
+  });
+  window.onclick = (e) => {
+    if (e.target === document.getElementById('Modal')) {
+      document.getElementById('Modal').style.display = 'none';
+    }
+  };
 };
