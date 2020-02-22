82662d6375ae40bd90c7cb7993368a5545bb992d
AlexRajya <55755818+AlexRajya@users.noreply.github.com>
2020-02-22 11:37:27 +0000

    register added

--- Public/index.js
+++ Public/index.js
@@ -5,15 +5,39 @@ window.onload = () => {
     results.textContent = document.getElementById('searchField').value;//Replace with api result when given API keys
   });
 
+  //Login modal listeners
   document.getElementById('login').addEventListener('click', () => {
-    document.getElementById('Modal').style.display = 'block';
+    document.getElementById('loginModal').style.display = 'block';
   });
-  document.getElementById('close').addEventListener('click', () => {
-    document.getElementById('Modal').style.display = 'none';
+  document.getElementById('loginClose').addEventListener('click', () => {
+    document.getElementById('loginModal').style.display = 'none';
   });
   window.onclick = (e) => {
-    if (e.target === document.getElementById('Modal')) {
-      document.getElementById('Modal').style.display = 'none';
+    if (e.target === document.getElementById('loginModal')) {
+      document.getElementById('loginModal').style.display = 'none';
     }
   };
+  //Register modal listeners
+  document.getElementById('register').addEventListener('click', () => {
+    document.getElementById('registerModal').style.display = 'block';
+  });
+  document.getElementById('registerClose').addEventListener('click', () => {
+    document.getElementById('registerModal').style.display = 'none';
+  });
+  window.onclick = (e) => {
+    if (e.target === document.getElementById('registerModal')) {
+      document.getElementById('registerModal').style.display = 'none';
+    }
+  };
+  //Login/register button listeners
+  document.getElementById('loginButton').addEventListener('click', login);
+  document.getElementById('registerButton').addEventListener('click', register);
 };
+
+function login(){
+  console.log("binnie smells")
+}
+
+function register(){
+
+}
