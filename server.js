d322dfdd656f281c0c09eb055264eb2a76a11a45
AlexRajya <55755818+AlexRajya@users.noreply.github.com>
2020-02-25 15:54:42 +0000

    Add files via upload

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-
 // Libraries
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const http = require('http');
 const ip = require('ip');
+const {OAuth2Client} = require('google-auth-library');
+var request = require('google-oauth-jwt')
 
 // constants
+const client = new OAuth2Client("3211205636-imtck5nq3h009s9fq6r9pcao79kbd2if");
 const port = process.env.PORT || 8080;
 const app = express();
 const server = http.createServer(app);
@@ -18,20 +20,33 @@ app.use(bodyParser.json());
 
 app.post('/login', (req, res) => {
   try {
-    fs.writeFileSync('basketData.txt', JSON.stringify(req.body));
+    const data = fs.readFileSync('basketData.txt', 'utf8')
+
+    let token = req.body;
+    verify(token.token);
+    //fs.writeFileSync('basketData.txt', JSON.stringify(req.body));
     res.sendStatus(200);// OK
-    console.log(JSON.stringify(req.body));
+    //console.log(JSON.stringify(req.body));
   } catch (err) {
     res.sendStatus(400);// bad request
   }
 });
+
+async function verify(token){
+  let ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: "3211205636-imtck5nq3h009s9fq6r9pcao79kbd2if"
+  });
+  let payload = ticket.getPayload();
+}
+
 
 // start the server
 server.listen(port, () => {
   console.log('Server started on:', `http://${ip.address()}:${port}`, 'or: http://localhost:8080/');
 });
 
-//create users file with default admin if none exists
+//create files with default values if none exists
 fs.exists('users.txt', function(exists) {
   if (!exists) {
     const admin = {
@@ -39,5 +54,15 @@ fs.exists('users.txt', function(exists) {
       password: ['12345']
     };
     fs.writeFileSync('users.txt', JSON.stringify(admin));
+  }
+});
+
+fs.exists('basketData.txt', function(exists) {
+  if (!exists) {
+    const baskets = {
+      user: ['id'],
+      basket: ['data']
+    };
+    fs.writeFileSync('basketData.txt', JSON.stringify((baskets)));
   }
 });
