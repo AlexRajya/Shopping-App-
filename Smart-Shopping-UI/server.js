5aa0c433b974a34e7b55be2bc279e56e8f102314
maahidev <61282558+maahidev@users.noreply.github.com>
2020-03-03 18:47:33 +0000

    configured server file

---
+++ Smart-Shopping-UI/server.js
@@ -0,0 +1,73 @@
+// Libraries
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const http = require('http');
+const hostname = "0.0.0.0";
+const ip = require('ip');
+var bby = require('bestbuy')('lfXY4GpC14duGk4N3uGvGD3d');
+
+// constants
+const port = process.env.PORT || 3000;
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.static(`${__dirname}/public`));
+app.use(express.static(__dirname));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//post requests
+app.post('/login', (req, res) => {
+  try {
+    let userEmail = (req.body).token;
+    loadUser(userEmail);
+    res.sendStatus(200);// OK
+  } catch (err) {
+    res.sendStatus(400);// bad request
+  }
+});
+
+// start the server
+server.listen(port, () => {
+  console.log('Server started on:', `http://${ip.address()}:${port}`, 'or: http://localhost:8080/');
+});
+//server functions
+function loadUser(email){
+  let data = fs.readFileSync('basketData.txt', 'utf8')
+  data = JSON.parse(data);
+  let pos = false;
+  for (let i = 0; i < data.user.length; i += 1) {
+    if (data.user[i] === email) {
+      pos = i;
+    }
+  }
+  if (pos === false){
+    console.log("user not found, appending to DB");
+    data.user.push(email);
+    data.basket.push(["basket:"])
+    fs.writeFileSync('basketData.txt', JSON.stringify(data));
+  }
+  //
+}
+
+//create files with default values if none exists
+fs.exists('users.txt', function(exists) {
+  if (!exists) {
+    const admin = {
+      username: ['admin'],
+      password: ['12345']
+    };
+    fs.writeFileSync('users.txt', JSON.stringify(admin));
+  }
+});
+
+fs.exists('basketData.txt', function(exists) {
+  if (!exists) {
+    const baskets = {
+      user: ['id','Admin'],
+      basket: [['product codes'],['1232232,231231123,2323442']]
+    };
+    fs.writeFileSync('basketData.txt', JSON.stringify((baskets)));
+  }
+});
