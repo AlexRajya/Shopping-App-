51d338ae6f8a5507bde7304d6aa01001809cbbe2
AlexRajya <55755818+AlexRajya@users.noreply.github.com>
2020-03-07 00:48:55 +0000

    Added loading current user's basket to basket page

---
+++ Public/js/basket.js
@@ -0,0 +1,121 @@
+window.onload = () => {
+  //Login modal listeners
+  document.getElementById('login').addEventListener('click', () => {
+    document.getElementById('loginModal').style.display = 'block';
+  });
+  document.getElementById('loginClose').addEventListener('click', () => {
+    document.getElementById('loginModal').style.display = 'none';
+  });
+  window.onclick = (e) => {
+    if (e.target === document.getElementById('loginModal')) {
+      document.getElementById('loginModal').style.display = 'none';
+    }
+  };
+  //Register modal listeners
+  document.getElementById('register').addEventListener('click', () => {
+    document.getElementById('registerModal').style.display = 'block';
+  });
+  document.getElementById('registerClose').addEventListener('click', () => {
+    document.getElementById('registerModal').style.display = 'none';
+  });
+  window.onclick = (e) => {
+    if (e.target === document.getElementById('registerModal')) {
+      document.getElementById('registerModal').style.display = 'none';
+    }
+  };
+  //Login/register button listeners
+  document.getElementById('loginButton').addEventListener('click', login);
+  document.getElementById('registerButton').addEventListener('click', register);
+
+};
+
+let currentUser;
+
+async function loadBasket(){
+  if(currentUser !== undefined){
+    const response = await fetch('basketData.txt');
+    const text = await response.text();
+    const basket = JSON.parse(text);
+    const table = document.querySelector("table");
+    for (let i = 0; i < basket.user.length; i += 1) {
+      if(basket.user[i] === currentUser){
+        generateTable(table,basket.basket[i]);
+        generateTableHead(table, Object.keys(basket.basket[i][0]))
+      }
+    }
+  }
+}
+
+async function login(){
+  const response = await fetch('users.txt');
+  const text = await response.text();
+  const users = JSON.parse(text);
+  const loginText = document.getElementById('loginText').value;
+  const passwordText = document.getElementById('passwordText').value;
+  for (let i = 0; i < users.username.length; i += 1) {
+    if (users.username[i] === loginText) {
+      if(users.password[i] === passwordText){
+        console.log("Successful login");
+        currentUser = loginText;
+      }else{
+        console.log("invalid login");
+      }
+    }
+  }
+}
+
+function register(){
+  //todo
+}
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId());
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  currentUser = profile.getEmail();
+  //send token to server
+  let id_token = googleUser.getAuthResponse().id_token;
+  const token = {token:profile.getEmail()};
+  const url = `${window.location.href}login`;
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', url, true);
+  xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+  xhr.send(JSON.stringify(token));
+  loadBasket();
+}
+
+function myInsertFunction() {
+  var table = document.getElementById("basketTable");
+  var row = table.insertRow(1);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  cell1.innerHTML = "NEW CELL1";
+  cell2.innerHTML = "NEW CELL2";
+}
+function myDeleteFunction() {
+  document.getElementById("basketTable").deleteRow(1);
+}
+
+function generateTableHead(table, data) {
+  let thead = table.createTHead();
+  let row = thead.insertRow();
+  for (let key of data) {
+    let th = document.createElement("th");
+    let text = document.createTextNode(key);
+    th.appendChild(text);
+    row.appendChild(th);
+  }
+}
+
+function generateTable(table, data) {
+  for (let element of data) {
+    let row = table.insertRow();
+    for (key in element) {
+      let cell = row.insertCell();
+      let text = document.createTextNode(element[key]);
+      cell.appendChild(text);
+    }
+  }
+}
