f4eb1acade881532ed50bac0583f3c6fc38d32a3
AlexRajya <55755818+AlexRajya@users.noreply.github.com>
2020-03-16 16:14:21 +0000

    Offers functionality

---
+++ Public/js/offers.js
@@ -0,0 +1,206 @@
+// Initialise
+async function getFromAPIs(search, vendor){
+  //tesco
+  let result;
+  console.log(vendor);
+  if (vendor === "Tesco"){
+    const tesco = await fetch( "https://dev.tescolabs.com/grocery/products/?query="+search+"&offset=0&limit=20&", {
+      method: 'GET',
+      headers: {
+        "Ocp-Apim-Subscription-Key": 'b1ef4204177a4deb86619436f9e1e7a6'
+      }
+    });
+    result = await tesco.json(); //extract JSON from the http response
+    result = result.uk.ghs.products.results;
+  }else if (vendor === "BestBuy"){
+    //bestBuy
+    const response = await fetch( 'https://api.bestbuy.com/v1/products(search='+search+'&onSale=true)?format=json&show=image,name,salePrice,regularPrice&apiKey=lfXY4GpC14duGk4N3uGvGD3d', {
+      method: 'GET',
+    });
+    result = await response.json(); //extract JSON from the http response
+  }else{
+    //Asda
+    const asda = await fetch( "https://cors-anywhere.herokuapp.com/https://groceries.asda.com/cmscontent/v2/items/autoSuggest?requestorigin=gi&searchTerm="+search+"&cacheable=true&storeId=4565&viewPort=xlarge", {
+      method: 'GET',
+    });
+    result = await asda.json(); //extract JSON from the http response
+    result = result.payload.autoSuggestionItems;
+  }
+  displayResults(result, vendor);
+}
+
+function displayResults(result, vendor){
+  let resultArea = document.getElementById('results');
+  resultArea.innerHTML = '';
+  let p,add,img;
+  p = document.createElement('p');
+  p.textContent = "Latest";
+  resultArea.appendChild(p);
+
+  p = document.createElement('p');
+  p.textContent = "Offers";
+  resultArea.appendChild(p);
+
+  p = document.createElement('p');
+  p.textContent = "From "+document.getElementById('vendors').value+":";
+  resultArea.appendChild(p);
+  let done;
+
+  for (let i = 1; i < 60; i += 1){
+    div = document.createElement('div');
+    p = document.createElement('p');
+    img = document.createElement('img');
+    add = document.createElement('button');
+    if (vendor === "BestBuy"){//If bestbuy
+      if (i < result.products.length){
+        p.textContent = result.products[i].name + " - Price: Now: £" + result.products[i].salePrice + " Was: £" + result.products[i].regularPrice;
+        img.src = result.products[i].image;
+      }else{
+        p.textContent = "No item found";
+        done = true;
+      }
+    }else if(vendor === "Tesco"){//If Tesco
+      if (i < result.length){
+        p.textContent = result[i].name + " - Price: £" + (result[i].price).toFixed(2);
+        img.src = result[i].image;
+      }else{
+        p.textContent = "No item found";
+        done = true;
+      }
+    }else{
+      if (i < result.length){
+        p.textContent = result[i].skuName + " - Price: " + result[i].price;
+        img.src = "https://ui.assets-asda.com"+result[i].extraLargeImageURL;
+      }else{
+        p.textContent = "No item found";
+        done = true;
+      }
+    }
+    if (done === true){
+      break;
+    }
+    add.textContent = "Add to basket";
+    add.addEventListener('click', addToBasket);
+
+    div.appendChild(img);
+    div.appendChild(p);
+    div.appendChild(add);
+    resultArea.appendChild(div);
+  }
+}
+
+function addToBasket(e){
+  let element = e.target.parentElement;
+  let itemInfo = {
+    info: undefined,
+    img: undefined,
+  }
+
+  for (let i = 0; i < element.children.length; i+=1){
+    if (element.children[i].tagName === 'P'){
+      itemInfo.info = element.children[i].textContent;
+    }else if(element.children[i].tagName === 'IMG'){
+      itemInfo.img = element.children[i].src;
+    }
+  }
+  console.log(itemInfo);
+  if (currentUser !== undefined){
+    sendPInfo(itemInfo);
+  }
+}
+
+function sendPInfo(itemInfo){
+  let body = {
+    user: currentUser,
+    itemInfo: itemInfo,
+  };
+  const url = `http://localhost:8080/pinfo`;
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', url, true);
+  xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+  xhr.send(JSON.stringify(body));
+}
+
+window.onload = () => {
+  document.getElementById('vendors').value = "BestBuy";
+  try{
+    document.getElementById('submitButton').addEventListener('click', () => {
+      getFromAPIs(document.getElementById('searchField').value, document.getElementById('vendors').value);
+    });
+    document.getElementById('searchField').addEventListener('keydown', (e) => {
+      if (e.key === "Enter"){
+        getFromAPIs(document.getElementById('searchField').value, document.getElementById('vendors').value);
+      }
+    });
+  }catch (err){
+    //throw
+  }
+  //Login modal listeners
+  document.getElementById('login').addEventListener('click', () => {
+    document.getElementById('loginModal').style.display = 'block';
+  });
+  document.getElementById('loginClose').addEventListener('click', () => {
+    document.getElementById('loginModal').style.display = 'none';
+  });
+  window.onclick = (e) => {
+    if (e.target === document.getElementById('loginModal')) {
+      document.getElementById('loginModal').style.display = 'none';
+    }
+  };
+  //Register modal listeners
+  document.getElementById('register').addEventListener('click', () => {
+    document.getElementById('registerModal').style.display = 'block';
+  });
+  document.getElementById('registerClose').addEventListener('click', () => {
+    document.getElementById('registerModal').style.display = 'none';
+  });
+  window.onclick = (e) => {
+    if (e.target === document.getElementById('registerModal')) {
+      document.getElementById('registerModal').style.display = 'none';
+    }
+  };
+  //Login/register button listeners
+  document.getElementById('loginButton').addEventListener('click', login);
+  document.getElementById('registerButton').addEventListener('click', register);
+};
+
+let currentUser;
+
+async function login(){
+  const response = await fetch('users.txt');
+  const text = await response.text();
+  const users = JSON.parse(text);
+  const loginText = document.getElementById('loginText').value;
+  const passwordText = document.getElementById('passwordText').value;
+  for (let i = 0; i < users.username.length; i += 1) {
+    if (users.username[i] === loginText) {
+      if(users.password[i] === passwordText){
+        console.log("Successful login");
+        currentUser = loginText;
+      }else{
+        console.log("invalid login");
+      }
+    }
+  }
+}
+
+function register(){
+  //todo
+}
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId());
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  currentUser = profile.getEmail();
+  //send token to server
+  let id_token = googleUser.getAuthResponse().id_token;
+  const token = {token:profile.getEmail()};
+  const url = `http://localhost:8080/login`;
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', url, true);
+  xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+  xhr.send(JSON.stringify(token));
+}
