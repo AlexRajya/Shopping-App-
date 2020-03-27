da27f1949bbfea0c8ddfd9d2b1dee06731a3a51e
AlexRajya <55755818+AlexRajya@users.noreply.github.com>
2020-03-27 14:04:51 +0000

    Add files via upload

--- Public/js/index.js
+++ Public/js/index.js
@@ -220,7 +220,7 @@ function onSignIn(googleUser) {
   //send token to server
   let id_token = googleUser.getAuthResponse().id_token;
   const token = {token:profile.getEmail()};
-  const url = `http://localhost:8080/login`;
+  const url = `http://localhost:8080/glogin`;
   const xhr = new XMLHttpRequest();
   xhr.open('POST', url, true);
   xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
